b781c2ad34ca88876ccb008e01f52ae11ecaa4ad
Brendan Annable <brendan.annable@uon.edu.au>
2014-08-27 10:27:23 +1000

    Added some commenting (shock, horror!)

--- public/app/view/ViewportController.js
+++ public/app/view/ViewportController.js
@@ -5,24 +5,26 @@ Ext.define('MW.view.ViewportController', {
 	extend: 'Ext.app.ViewController',
 	alias: 'controller.ViewportController',
 	config: {
-		gl: null,
-		shaderProgram: null,
-		canvas: null,
-		mvStack: null,
-		mvMatrix: null,
-		pMatrix: null,
-		models: null,
-		lastTime: 0,
-		zenithAngle: 0,
-		azimuthAngle: 0
+		gl: null, // The webGL context
+		shaderProgram: null, // The shader program containing the vertex and fragment shader
+		canvas: null, // The HTML canvas used for drawing on
+		mvStack: null, // The model-view projection stack, used to keep a history of where you draw
+		mvMatrix: null, // The current model-view project matrix (where to draw)
+		pMatrix: null, // The perspective projection matrix
+		models: null, // A map of models that have been loaded, keyed by their name
+		lastTime: 0, // Used by the animate function, to keep track of the time between animation frames
+		zenithAngle: 0, // Spherical coordinates angle for pitch
+		azimuthAngle: 0 // Spherical coordinates angle for yaw
 	},
 	init: function () {
+		// Initialize any needed arrays or objects
 		this.setPMatrix(new Float32Array(16));
 		this.setMvMatrix(new Float32Array(16));
 		this.setMvStack([]);
 		this.setModels({});
 	},
 	onResize: function (container, width, height) {
+		// Resize the WebGL viewport based on the new size
 		var canvas = this.getCanvas();
 		var gl = this.getGl();
 		canvas.width = width;
@@ -31,9 +33,11 @@ Ext.define('MW.view.ViewportController', {
 		gl.viewportHeight = height;
 	},
 	mvPush: function () {
+		// Push a copy of the current model-view projection matrix on the stack
 		this.getMvStack().push(mat4.clone(this.getMvMatrix()));
 	},
 	mvPop: function () {
+		// Pop the latest model-view projection matrix off the stack
 		var mvStack = this.getMvStack();
 		if (mvStack.length === 0) {
 			throw "mvStack empty";
@@ -41,20 +45,27 @@ Ext.define('MW.view.ViewportController', {
 		this.setMvMatrix(mvStack.pop());
 	},
 	onAfterRender: function (container) {
+		// Called after the DOM has rendered the canvas element
 		var canvas = container.getEl().dom;
 		this.setCanvas(canvas);
 
+		// Setup WebGL
 		var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 		gl.viewportWidth = canvas.width;
 		gl.viewportHeight = canvas.height;
 		this.setGl(gl);
 
+		// Initialize the shaders
 		this.initShaders(gl, function (shaderProgram) {
 			this.setShaderProgram(shaderProgram);
+			// Load the face model
 			this.loadModel(gl, 'face.json', function () {
+				// Set the background color
 				gl.clearColor(0, 0, 0, 1);
+				// Enable depth testing
 				gl.enable(gl.DEPTH_TEST);
 
+				// Start the animation loop
 				this.tick();
 			});
 		});
@@ -65,20 +76,29 @@ Ext.define('MW.view.ViewportController', {
 		requestAnimationFrame(Ext.bind(this.tick, this));
 	},
 	animate: function () {
+		// Update any needed values based on the time elapsed
 		var now = Date.now();
 		var lastTime = this.getLastTime();
 		if (lastTime != 0) {
 			var zenithAngle = this.getZenithAngle();
 			var elapsed = now - lastTime;
+			// The zenith angle will have a period of 4 seconds
 			zenithAngle += (2 * Math.PI  * elapsed) / 4000;
 			this.setZenithAngle(zenithAngle);
 
 			var azimuthAngle = this.getAzimuthAngle();
+			// The azimuth angle will have a period of 1.5 seconds
 			azimuthAngle += (2 * Math.PI  * elapsed) / 1500;
 			this.setAzimuthAngle(azimuthAngle);
 		}
 		this.setLastTime(now);
 	},
+	/**
+	 * Draws the WebGL scene, must be called continuously to produce animations
+	 *
+	 * @param gl The WebGL context
+	 * @param shaderProgram The WebGL shader program
+	 */
 	drawScene: function (gl, shaderProgram) {
 		if (gl === undefined) {
 			gl = this.getGl();
@@ -89,77 +109,90 @@ Ext.define('MW.view.ViewportController', {
 		if (shaderProgram === null) {
 			return;
 		}
+
+		// Set the viewport size
 		gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+
+		// Clear the color buffer and depth buffer bits
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 		var mvMatrix = this.getMvMatrix();
 		var pMatrix = this.getPMatrix();
 
+		// Create a perspective projection matrix
 		mat4.perspective(pMatrix, 45 * Math.PI / 180 , gl.viewportWidth / gl.viewportHeight, 0.1, 100);
 
+		// Reset model-view project to the origin
 		mat4.identity(mvMatrix);
 
+		// Translate away from the camera
 		mat4.translate(mvMatrix, mvMatrix, [0, 0, -30]);
 
-		this.mvPush();
-//		mat4.rotateX(mvMatrix, mvMatrix, -Math.PI / 2);
-//		mat4.translate(mvMatrix, mvMatrix, [0, 1.5, 0]);
-		mat4.rotateY(mvMatrix, mvMatrix, this.getZenithAngle());
-		mat4.rotateX(mvMatrix, mvMatrix, -Math.PI / 2);
-//		mat4.rotateZ(mvMatrix, mvMatrix, this.getAngle());
-//		mat4.rotateX(mvMatrix, mvMatrix, this.getAngle());
-//		var triangleBuffer = this.getTriangleBuffer();
-//		gl.bindBuffer(gl.ARRAY_BUFFER, triangleBuffer);
-//		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
-//		var triangleColorBuffer = this.getTriangleColorBuffer();
-//		gl.bindBuffer(gl.ARRAY_BUFFER, triangleColorBuffer);
-//		gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, triangleColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
-//		this.setMatrixUniforms(gl, shaderProgram);
-
-//		gl.drawArrays(gl.TRIANGLES, 0, triangleBuffer.numItems);
-
+		// Get the models map
 		var models = this.getModels();
+
+		// Start drawing the face
 		var face = models.face;
 
+		// Save current position on the stack
+		this.mvPush();
+
+		// Animate the face around the Y axis
+		mat4.rotateY(mvMatrix, mvMatrix, this.getZenithAngle());
+
+		// Rotate the face so that it faces the camera
+		mat4.rotateX(mvMatrix, mvMatrix, -Math.PI / 2);
+
+		// Update the face position
 		var vertexBuffer = face.vertexBuffer;
 		gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
+		// Update the face normals
 		var normalBuffer = face.normalBuffer;
 		gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
 		gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, normalBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
+		// Update the face faces (ha)
 		var faceBuffer = face.faceBuffer;
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, faceBuffer);
 
+		// Update the WebGL uniforms
 		this.setMatrixUniforms(gl, shaderProgram);
 
+		// Draw the face to the scene
 		gl.drawElements(gl.TRIANGLES, faceBuffer.numItems * faceBuffer.itemSize, gl.UNSIGNED_SHORT, 0);
 
+		// Restore original position
 		this.mvPop();
 	},
 	setMatrixUniforms: function (gl, shaderProgram) {
 		var pMatrix = this.getPMatrix();
 		var mvMatrix = this.getMvMatrix();
+		// Send the projection and model-view matrices to WebGL
 		gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
 		gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 
+		// Send the model-view project normal matrix to WebGL
 		var normalMatrix = new Float32Array(9);
 		mat3.normalFromMat4(normalMatrix, mvMatrix);
 		gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, normalMatrix);
 
+		// Convert from spherical to cartesian coordinates
 		var dist = 1000;
 		var zenithAngle = this.getZenithAngle();
 		var azimuthAngle = this.getAzimuthAngle();
 		var x = dist * Math.sin(zenithAngle) * Math.cos(azimuthAngle);
 		var y = dist * Math.cos(zenithAngle);
 		var z = dist * Math.sin(zenithAngle) * Math.sin(azimuthAngle);
+		// Send the light position and color to WebGL
 		gl.uniform3fv(shaderProgram.uLightPos, [x, y, z]);
 		gl.uniform3fv(shaderProgram.uLightColor, [0.6, 0, 0]);
 	},
 	initShaders: function (gl, callback) {
+		// Initialize the shaders
 		this.loadShaders(gl, function (vertexShader, fragmentShader) {
+			// Create a WebGL shader program
 			var shaderProgram = gl.createProgram();
 			gl.attachShader(shaderProgram, vertexShader);
 			gl.attachShader(shaderProgram, fragmentShader);
@@ -171,6 +204,7 @@ Ext.define('MW.view.ViewportController', {
 
 			gl.useProgram(shaderProgram);
 
+			// Setup the attributes
 			shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
 			gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 
@@ -180,6 +214,7 @@ Ext.define('MW.view.ViewportController', {
 //			shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
 //			gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
 
+			// Setup the uniforms
 			shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
 			shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 			shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
@@ -198,11 +233,16 @@ Ext.define('MW.view.ViewportController', {
 			url: url,
 			scope: this,
 			success: function (response) {
+				// Decode the JSON response
 				var model = Ext.decode(response.responseText);
+				// Get the meshes array
 				var meshes = model.meshes;
+				// Loop meshes array
 				Ext.each(meshes, function (mesh) {
+					// Create a geometry object
 					var geom = Ext.create('MW.util.Geometry');
 					var vertices = [];
+					// Loop through the flat vertices array and convert to proper Vector3 objects
 					for (var i = 0; i < mesh.vertices.length; i += 3) {
 						vertices.push(Vector.create([
 							mesh.vertices[i + 0],
@@ -210,9 +250,13 @@ Ext.define('MW.view.ViewportController', {
 							mesh.vertices[i + 2]
 						]));
 					}
+					// Update the geometry vertices
 					geom.setVertices(vertices);
+
+					// Move the model's pivot point to the center of the model
 					geom.center();
 
+					// Create the WebGL buffers for the vertices, normals and faces
 					var vertexBuffer = gl.createBuffer();
 					gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 					var vertices = geom.getFlattenedVertices();
@@ -246,6 +290,7 @@ Ext.define('MW.view.ViewportController', {
 						faceBuffer: faceBuffer
 					};
 
+					// Store into the models map
 					models[mesh.name] = model;
 
 					callback.call(this, model);
@@ -256,6 +301,7 @@ Ext.define('MW.view.ViewportController', {
 	loadShaders: function (gl, callback) {
 		var shaderPath = Ext.Loader.getPath('MW') + '/shader/';
 
+		// Load the vertex and fragment shader
 		this.loadVertexShader(gl, shaderPath + 'vertex.c', function (vertexShader) {
 			this.loadFragmentShader(gl, shaderPath + 'fragment.c', function (fragmentShader) {
 				callback.call(this, vertexShader, fragmentShader);
