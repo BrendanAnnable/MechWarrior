bb6e114cdfad43ef723a060bb9915ff64c1716e3
Julius Sky <julius.skye@gmail.com>
2014-09-28 01:50:54 +1000

    cleaned up abit so that similar asset promises in same method, then configged, as opposed to many differently configged promises.

--- public/app/scene/assets/Global.js
+++ public/app/scene/assets/Global.js
@@ -12,32 +12,56 @@ Ext.define('MW.scene.assets.Global', {
 	 */
 	load: function (assetManager) {
 		return Promise.all([
-            this.loadPlayerAsset(this.getModelPath('mech.json')).then(function (player) {
-			    assetManager.addAsset('player', player);
+
+        //load models
+            this.loadModelAsset(this.getModelPath('mech.json')).then(function (player) {
+			    assetManager.addAsset('player', player); // addAsset(key,Asset)
+                player.name = 'player';
+                player.material.getColor().setR(0);
+                player.material.getColor().setG(1);
+                player.material.getColor().setB(0);
 		    }),
-		    this.loadBulletAsset(this.getModelPath('bullet.json')).then(function (bullet) {
+		    this.loadModelAsset(this.getModelPath('bullet.json')).then(function (bullet) {
 			    assetManager.addAsset('bullet', bullet);
+                bullet.name = 'bullet';
                 bullet.geometry.scale(vec3.fromValues(5, 5, 5));
 		    }),
+
+            this.loadModelAsset(this.getModelPath('face.json')).then(function (face) {
+                assetManager.addAsset('face', face);
+                face.name = 'face';
+                face.geometry.scale([0.05, 0.05, 0.05]);
+				face.geometry.translate([0, 2, 0]);
+				face.geometry.rotateY(Math.PI);
+            }),
+
+            this.loadModelAsset(this.getModelPath('destroyedCar.json')).then(function (face) {
+                assetManager.addAsset('destroyedCar', destroyedCar);
+                destroyedCar.name = 'destroyedCar';
+                destroyedCar.geometry.scale([0.05, 0.05, 0.05]);
+                destroyedCar.geometry.translate([0, 2, 0]);
+                destroyedCar.geometry.rotateY(Math.PI);
+            }),
+
+        // load sounds
 	        this.loadSoundAsset(this.getSoundPath('bullet.mp3')).then(function (sound) {
 		        assetManager.addAsset('bulletSound', sound);
-	        }),
-			this.loadFaceAsset(this.getModelPath('face.json')).then(function (face) {
-				assetManager.addAsset('face', face);
-			})
+	        })
+
         ]);
 	},
+    
 	/**
-	 * Loads the player model and returns it as a promise once it has finished loading.
+	 * Loads a model asset and returns it as a promise once it has finished loading.
 	 *
-	 * @param url The path of the player model to load.
+	 * @param url The path of the model to load.
 	 * @returns {Promise}
 	 */
-	loadPlayerAsset: function (url) {
+	loadModelAsset: function (url) {
 		return new Promise(function (resolve) {
 			Ext.create('FourJS.loader.Model').load(url).then(function (geometry) {
 				resolve ({
-					name: 'Player',
+					name: 'name',
 					geometry: geometry,
 					material: Ext.create('FourJS.material.Phong', {
 						color: Ext.create('FourJS.util.Color', {
@@ -50,65 +74,19 @@ Ext.define('MW.scene.assets.Global', {
 			});
 		});
 	},
-	/**
-	 * Loads the face model and returns it as a promise once it has finished loading.
-	 *
-	 * @param url The path of the face model to load.
-	 * @returns {Promise}
-	 */
-	loadFaceAsset: function (url) {
-		return new Promise(function (resolve) {
-			Ext.create('FourJS.loader.Model').load(url).then(function (geometry) {
-				geometry.scale([0.05, 0.05, 0.05]);
-				geometry.translate([0, 2, 0]);
-				geometry.rotateY(Math.PI);
-				resolve ({
-					name: 'Face',
-					geometry: geometry,
-					material: Ext.create('FourJS.material.Phong', {
-						color: Ext.create('FourJS.util.Color', {
-							r: 0,
-							g: 1,
-							b: 0
-						})
-					})
-				});
-			});
-		});
-	},
+
     /**
-     * Loads the bullet model and returns it as a promise once it has finished loading.
+     * Loads a sound asset for the game.
      *
-     * @param url The path of the bullet model to load.
+     * @param url The url to load the sound from
      * @returns {Promise}
      */
-    loadBulletAsset: function (url) {
-        return new Promise(function (resolve) {
-            Ext.create('FourJS.loader.Model').load(url).then(function (geometry) {
-                resolve ({
-                    name: 'Bullet',
-                    geometry: geometry,
-                    material: Ext.create('FourJS.material.Phong', {
-                        color: Ext.create('FourJS.util.Color', {
-                            r: 0,
-                            g: 1,
-                            b: 0
-                        })
-                    })
-                });
-            });
-        });
-    },
-	/**
-	 * Loads a sound asset for the game.
-	 *
-	 * @param url The url to load the sound from
-	 * @returns {Promise}
-	 */
-	loadSoundAsset: function (url) {
-		return Promise.resolve(soundManager.createSound({
-			url: url,
-			multiShot: true
-		}));
-	}
+    loadSoundAsset: function (url) {
+        return Promise.resolve(soundManager.createSound({
+            url: url,
+            multiShot: true
+        }));
+    }
+
+
 });
